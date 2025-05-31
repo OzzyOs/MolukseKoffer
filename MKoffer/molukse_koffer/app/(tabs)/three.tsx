54b73722da72fc931f0440b4d2c136730b835ead
OzzyOs <joshuahursepuny@gmail.com>
2025-05-31 17:22:14 +0200

    Updated packages with Maps. Implemented Map feature.

--- MKoffer/molukse_koffer/app/(tabs)/three.tsx
+++ MKoffer/molukse_koffer/app/(tabs)/three.tsx
@@ -4,7 +4,7 @@ export default function TabThreeScreen() {
   return (
     <View flex={1} items="center" justify="center" bg="$background">
       <Text fontSize={20} color="$blue10">
-        Tab Two
+        Lineage
       </Text>
     </View>
   );
--- MKoffer/molukse_koffer/app/(tabs)/two.tsx
+++ MKoffer/molukse_koffer/app/(tabs)/two.tsx
@@ -1,11 +1,20 @@
 import { Text, View } from 'tamagui'
+import MapView from 'react-native-maps';
+import { Platform } from 'react-native';
+
 
 export default function TabTwoScreen() {
   return (
     <View flex={1} items="center" justify="center" bg="$background">
-      <Text fontSize={20} color="$blue10">
-        Tab Two
-      </Text>
+    <MapView
+      style={{ flex: 1, height: '100%', width: '100%' }}
+      initialRegion={{
+      latitude: 51.9225,
+      longitude: 4.4792,
+      latitudeDelta: 0.05,
+      longitudeDelta: 0.08,
+      }}
+    />
     </View>
   )
 }
